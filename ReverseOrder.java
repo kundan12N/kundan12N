9fe4a8486b6cb6a4c975f256ea49012364355d40
kundan12N <153602813+kundan12N@users.noreply.github.com>
2023-12-26 14:55:44 +0530

    Create ReverseOrder.java

---
+++ ReverseOrder.java
@@ -0,0 +1,24 @@
+//Program to reverse the order of digits in a given number
+import java.util.Scanner;
+public class ReverseOrder
+{
+   public static void main(String args[])
+	{
+	  Scanner reader = new Scanner (System.in);
+	  System.out.print("\nEnter the number: ");
+	  int num = reader.nextInt();
+	  int numcopy = num;
+	  int reversenum = 0;
+	  while(num >0)
+	  {
+	    reversenum = reversenum*10 + num%10;
+	    num = num/10;
+	    System.out.print(num);
+	  }	
+	     System.out.print("\nReverse of the given number is " + reversenum); 
+	}
+}
+// out put:
+//Enter the number: 456
+//4540
+//Reverse of the given number is 654
